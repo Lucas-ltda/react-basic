f3b2ca0f09ce1bcd4958843a60547f85f0f74628
Lucas <lucaslksleite@gmail.com>
2026-03-27 21:31:10 -0300

    3.41 - PrevStates

--- secao3/avancado/src/components/ListRender.js
+++ secao3/avancado/src/components/ListRender.js
@@ -2,14 +2,22 @@ import { useState } from "react"
 
 const ListRender = () => {
 
-    const [users] = useState([
+    const [users,setUsers] = useState([
         { id: 1, nome: "Lucas", descrição: "Desenvolvedor React" },
         { id: 2, nome: "Maria", descrição: "Designer UX/UI" },
         { id: 3, nome: "João", descrição: "Engenheiro Backend" },
         { id: 4, nome: "Ana", descrição: "Product Manager" }
     ])
-    
 
+    
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * users.length)+1;
+
+        setUsers((prevUsers) => {
+            return  prevUsers.filter((user) => randomNumber !== user.id)
+        });
+    }
+ 
     return(
         <div>
             <ul>
@@ -18,6 +26,10 @@ const ListRender = () => {
                     <li key = {user.id}>{user.nome} - {user.descrição}</li>
                 ) )}
             </ul>
+
+            <button onClick={deleteRandom}>
+                Clique Aqui Bonitinho!
+            </button>
         </div>
     )
 }
