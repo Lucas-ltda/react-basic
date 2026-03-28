3e5bf3e6c298c1cb9c0d1d648fd113da602ae943
Lucas <lucaslksleite@gmail.com>
2026-03-28 11:44:44 -0300

    3.47 - Renderização de lista em componente

--- secao3/avancado/src/App.js
+++ secao3/avancado/src/App.js
@@ -9,6 +9,13 @@ import ManageData from './components/ManageData';
 function App() {
   const userName  = "Lucas"
   
+  // crie um array de objetos com os detalhes de carros com id
+  const cars = [
+    { id: 1, brand: "Ferrai", km: 10000, color: "Vermelho", newCar: false },
+    { id: 2, brand: "Porche", km: 0, color: "Azul", newCar: true },
+    { id: 3, brand: "Lamborghini", km: 5000, color: "Prata", newCar: false }
+  ];
+
 
   return (
     <div className="App">
@@ -45,6 +52,19 @@ function App() {
       <CarDetails brand = "GmGroup" km = {10000} color = "Azul" newCar ={false} />
       <CarDetails brand = "VW" km = {0} color = "Vermelho" newCar ={true} />
       <CarDetails brand = "Toyota" km = {5000} color = "Prata" newCar ={false} />
+    </div>
+    {/* loop de array de objetos  */}
+    <div>
+      <h2>Loop de array com objetos</h2>
+      {
+        cars.map((car) =>(
+          <CarDetails 
+          brand = {car.brand} 
+          km = {car.km} 
+          color = {car.color} 
+          newCar={car.km} />
+        ))
+      }
     </div>
   </div>
     
